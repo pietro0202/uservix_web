03b85cb6585f260fd6e9c7047d2e8e0d9cb3c6c4
poti02 <74553771+pietro0202@users.noreply.github.com>
2026-03-13 20:28:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-document.querySelectorAll("form").forEach(form => {
+const forms = document.querySelectorAll("form")
 
-form.addEventListener("submit", function(e){
+forms.forEach(form =>{
+
+form.addEventListener("submit",(e)=>{
 
 e.preventDefault()
 
@@ -9,3 +11,56 @@ alert("Formulario enviado")
 })
 
 })
+
+/* BOTON MODO OSCURO */
+
+const boton = document.getElementById("modoBtn")
+const logo = document.getElementById("logoImg")
+
+/* CARGAR MODO GUARDADO */
+
+if(localStorage.getItem("modo") === "oscuro"){
+
+document.body.classList.add("dark-mode")
+
+if(logo){
+logo.src = "images/logouservixblanco.png"
+}
+
+}else{
+
+if(logo){
+logo.src = "images/logouservixnegro.png"
+}
+
+}
+
+/* CAMBIAR MODO */
+
+if(boton){
+
+boton.addEventListener("click", () =>{
+
+document.body.classList.toggle("dark-mode")
+
+if(document.body.classList.contains("dark-mode")){
+
+localStorage.setItem("modo","oscuro")
+
+if(logo){
+logo.src = "images/logouservixblanco.png"
+}
+
+}else{
+
+localStorage.setItem("modo","claro")
+
+if(logo){
+logo.src = "images/logouservixnegro.png"
+}
+
+}
+
+})
+
+}
